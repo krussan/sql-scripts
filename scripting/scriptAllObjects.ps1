15af9a3c932a425772af8a4edb58940440d6a09d
Christian Thunström <christian.thunstrom@nordax.se>
2018-09-13 09:02:46 +0200

    Updated script with parameters

--- scripting/scriptAllObjects.ps1
+++ scripting/scriptAllObjects.ps1
@@ -1,37 +1,54 @@
-[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-$serverInstance = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $ServerName
+param(
+	[string]$server,
+	[string]$database,
+	[string]$outputpath
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
+$srv = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $server
  
-$IncludeTypes = @("tables", "StoredProcedures", "Views", "UserDefinedFunctions")
+$IncludeTypes = @("Tables", "StoredProcedures", "Views", "UserDefinedFunctions", "Synonyms", "UserDefinedDataTypes", "UserDefinedTableTypes", "Triggers", "PartitionSchemes", "PartitionFunctions")
 $ExcludeSchemas = @("sys", "Information_Schema")
  
  
 $so = new-object ('Microsoft.SqlServer.Management.Smo.ScriptingOptions')
-$so.IncludeIfNotExists = 0
-$so.SchemaQualify = 1
-$so.AllowSystemObjects = 0
-$so.ScriptDrops = 0 #Script Drop Objects
- 
-$dbs = $serverInstance.Databases
-foreach ($db in $dbs)
+#$so.IncludeIfNotExists = 0
+#$so.SchemaQualify = 1
+#$so.AllowSystemObjects = 0
+#$so.ScriptDrops = 0 #Script Drop Objects
+$so.ScriptSchema = $true;
+$so.ScriptData = $false;
+$so.NoCommandTerminator = $false;
+$so.Indexes = $true;
+$so.ClusteredIndexes = $true;
+$so.DriAllKeys = $true;
+$so.ExtendedProperties = $true;
+$so.IncludeIfNotExists = $true;
+$so.NoCollation = $true;
+$so.Permissions = $false;
+$so.ScriptDataCompression = $true;
+$so.Triggers = $true;
+$so.XmlIndexes = $true;
+
+$db = $srv.Databases[$database]
+
+$dbname = "$db".replace("[", "").replace("]", "")
+$dbpath = "$outputpath" + "\" + "$dbname" + "\"
+if (!(Test-Path $dbpath))
+{ $null = new-item -type directory -name "$dbname" -path "$outputpath" }
+
+foreach ($Type in $IncludeTypes)
 {
-    $dbname = "$db".replace("[", "").replace("]", "")
-    $dbpath = "$path" + "$dbname" + "\"
-    if (!(Test-Path $dbpath))
-    { $null = new-item -type directory -name "$dbname" -path "$path" }
-    
-    foreach ($Type in $IncludeTypes)
-    {
-        $objpath = "$dbpath" + "$Type" + "\"
-        if (!(Test-Path $objpath))
-        { $null = new-item -type directory -name "$Type" -path "$dbpath" }
-        foreach ($objs in $db.$Type)
-        {
-            If ($ExcludeSchemas -notcontains $objs.Schema)
-            {
-                $ObjName = "$objs".replace("[", "").replace("]", "")
-                $OutFile = "$objpath" + "$ObjName" + ".sql"
-                $objs.Script($so) + "GO" | out-File $OutFile #-Append
-            }
-        }
-    }
+	$objpath = "$dbpath" + "$Type" + "\"
+	if (!(Test-Path $objpath))
+	{ $null = new-item -type directory -name "$Type" -path "$dbpath" }
+	foreach ($objs in $db.$Type)
+	{
+		If ($ExcludeSchemas -notcontains $objs.Schema)
+		{
+			$ObjName = "$objs".replace("[", "").replace("]", "")
+			$OutFile = "$objpath" + "$ObjName" + ".sql"
+			$objs.Script($so) + "GO" | out-File $OutFile #-Append
+		}
+	}
 }
